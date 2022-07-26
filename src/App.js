8d967903256fb8ac16ca353e95461ea83b87d808
veronika <hunyakvera@gmail.com>
2022-07-26 21:05:48 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavigationBar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar />
     </div>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,19 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavigationBar() {
+  return (
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">Simple Mock App</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
